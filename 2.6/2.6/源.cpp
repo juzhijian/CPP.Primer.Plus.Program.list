88fc4a15fff948e22ed599a10a572f7a12f5c0cc
郭金鑫 <princergx@live.com>
2015-09-26 21:49:01 +0800

    添加程序清单2.6

---
+++ 2.6/2.6/源.cpp
@@ -0,0 +1,21 @@
+﻿//源.cpp -- converts stone to pounds
+#include<iostream>
+int stonetold(int);					//function prototype
+int main()
+{
+	using namespace std;
+	int stone;
+	cout << "Enter the weight in stone:";
+	cin >> stone;
+	int pounds = stonetold(stone);
+	cout << stone << "stone=";
+	cout << pounds << "pounds" << endl;
+	cin.get();
+	cin.get();
+	return 0;
+}
+
+int stonetold(int sts)
+{
+	return 14 * sts;
+}
