1029ce9d8dc33287583a37ed1413c84a33e3269c
郭金鑫 <princergx@live.com>
2015-10-14 12:09:38 +0800

    添加程序清单3.3、3.4、3.5

---
+++ Visual Studio 2015/第3章程序清单/3.3/3.3.cpp
@@ -0,0 +1,17 @@
+﻿//3.3.CPP -- shows hex and octal liteals
+#include <iostream>
+int main()
+{
+	using namespace std;
+	int chest = 42;				//decimal integer literal
+	int waist = 0x42;			//hexadecimal integer literal
+	int inseam = 042;			//octal integer literal
+
+	cout << "Monsieur cute a striking figure!\n";
+	cout << "chest = " << chest << "(42 in decimal)\n";
+	cout << "waist = " << waist << "(0x42 in hex)\n";
+	cout << "inseam = " << inseam << "(042 in octal)\n";
+	cin.get();
+	cin.get();
+	return 0;
+}
---
+++ Visual Studio 2015/第3章程序清单/3.4/3.4.cpp
@@ -0,0 +1,20 @@
+﻿//3.4.CPP -- dispaly values in hex and octal
+#include <iostream>
+using namespace std;
+int main()
+{
+	using namespace std;
+	int chest = 42;
+	int waist = 42;
+	int inseam = 42;
+
+	cout << "Monsieur cuts a striking figure!" << endl;
+	cout << "chest = " << chest << "(decimal of 42)" << endl;
+	cout << hex;							//manipulator for changing number base
+	cout << "waist = " << waist << "(hexadecimal for 42)" << endl;
+	cout << oct;							//manipulator for changing number base
+	cout << "inseam = " << inseam << "(octal for 42)" << endl;
+	cin.get();
+	cin.get();
+	return 0;
+}
